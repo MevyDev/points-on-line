88cce6bc7ca53a717e18471cea93eb8e3d9d45b3
MevyDev <mevydev@proton.me>
2025-06-08 14:05:14 +0200

    fix: exclusive max grid size

--- input_gen.py
+++ input_gen.py
@@ -5,8 +5,8 @@ def generate_input(x_size, y_size, point_count):
     points_used = set()
 
     for i in range(point_count):
-        x = random.randint(0, x_size)
-        y = random.randint(0, y_size)
+        x = random.randint(0, x_size - 1)
+        y = random.randint(0, y_size - 1)
         points_used.add((x, y))
 
     return list(points_used)
