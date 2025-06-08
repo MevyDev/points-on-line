3027f8ad535c6f7d7b1045f88ee697785edc90c4
MevyDev <mevydev@proton.me>
2025-06-08 14:09:57 +0200

    fix: ensure correct point count

--- input_gen.py
+++ input_gen.py
@@ -4,7 +4,7 @@ import random
 def generate_input(x_size, y_size, point_count):
     points_used = set()
 
-    for i in range(point_count):
+    while len(points_used) != point_count:
         x = random.randint(0, x_size - 1)
         y = random.randint(0, y_size - 1)
         points_used.add((x, y))
