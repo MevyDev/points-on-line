4df976d6f5e07d4a7b398c271adfd7e78f36202a
MevyDev <mevydev@proton.me>
2025-06-08 11:33:27 +0200

    refactor: rename variables

--- input_gen.py
+++ input_gen.py
@@ -1,12 +1,12 @@
 import random
 
 
-def generate_input(max_x, max_y, point_count):
+def generate_input(x_size, y_size, point_count):
     points_used = set()
 
     for i in range(point_count):
-        x = random.randint(0, max_x)
-        y = random.randint(0, max_y)
+        x = random.randint(0, x_size)
+        y = random.randint(0, y_size)
         points_used.add((x, y))
 
     return list(points_used)
