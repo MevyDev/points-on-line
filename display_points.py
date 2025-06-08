021706a6a8c424ab7a4a1c4ff44a2e8d9ef68236
MevyDev <mevydev@proton.me>
2025-06-08 15:44:33 +0200

    refactor: put grid_size touple into own values

--- display_points.py
+++ display_points.py
@@ -3,10 +3,12 @@ from constants import WINDOW_SIZE, WINDOW_MARGIN
 
 
 def draw_points(grid_size, points, lines):
-    ratio = grid_size[0] / grid_size[1]
-    ratio = max(ratio, grid_size[1] / grid_size[0])
+    grid_size_x, grid_size_y = grid_size
 
-    if grid_size[0] > grid_size[1]:
+    ratio = grid_size_x / grid_size_y
+    ratio = max(ratio, grid_size_y / grid_size_x)
+
+    if grid_size_x > grid_size_y:
         x_window = WINDOW_SIZE
         y_window = round(WINDOW_SIZE / ratio)
     else:
@@ -26,18 +28,18 @@ def draw_points(grid_size, points, lines):
     )
     canvas.pack()
 
-    radius = max(1, (min(x_window // grid_size[0], y_window // grid_size[1])) // 2)
+    radius = max(1, (min(x_window // grid_size_x, y_window // grid_size_y)) // 2)
 
-    first_x = round((points[0][0] / grid_size[0]) * x_window) + WINDOW_MARGIN
-    first_y = round((points[0][1] / grid_size[1]) * y_window) + WINDOW_MARGIN
+    first_x = round((points[0][0] / grid_size_x) * x_window) + WINDOW_MARGIN
+    first_y = round((points[0][1] / grid_size_y) * y_window) + WINDOW_MARGIN
     for x, y in points:
-        rel_x = round((x / grid_size[0]) * x_window) + WINDOW_MARGIN
-        rel_y = round((y / grid_size[1]) * y_window) + WINDOW_MARGIN
+        rel_x = round((x / grid_size_x) * x_window) + WINDOW_MARGIN
+        rel_y = round((y / grid_size_y) * y_window) + WINDOW_MARGIN
         canvas.create_line(rel_x, rel_y, first_x, first_y, fill="blue", width=2)
 
     for x, y in lines:
-        rel_x = round((x / grid_size[0]) * x_window) + WINDOW_MARGIN
-        rel_y = round((y / grid_size[1]) * y_window) + WINDOW_MARGIN
+        rel_x = round((x / grid_size_x) * x_window) + WINDOW_MARGIN
+        rel_y = round((y / grid_size_y) * y_window) + WINDOW_MARGIN
         if (x, y) in points:
             canvas.create_oval(
                 rel_x - radius,
