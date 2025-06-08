515d90c3f8aadd17b464ac8bb457a3a30d40e6e6
MevyDev <mevydev@proton.me>
2025-06-08 14:17:14 +0200

    refactor: merge point creation into one loop

--- display_points.py
+++ display_points.py
@@ -11,19 +11,15 @@ def draw_points(grid_size, points, lines):
     canvas.pack()
 
     for x, y in lines:
-        if (x, y) in points:
-            continue
         rel_x = round((x / grid_size[0]) * WINDOW_SIZE[0])
         rel_y = round((y / grid_size[1]) * WINDOW_SIZE[1])
+        if (x, y) in points:
+            canvas.create_oval(
+                rel_x - 2, rel_y - 2, rel_x + 2, rel_y + 2, fill="red", outline="red"
+            )
+            continue
         canvas.create_oval(
             rel_x - 2, rel_y - 2, rel_x + 2, rel_y + 2, fill="black", outline="black"
-        )
-
-    for x, y in points:
-        rel_x = round((x / grid_size[0]) * WINDOW_SIZE[0])
-        rel_y = round((y / grid_size[1]) * WINDOW_SIZE[1])
-        canvas.create_oval(
-            rel_x - 2, rel_y - 2, rel_x + 2, rel_y + 2, fill="red", outline="red"
         )
 
     root.mainloop()
