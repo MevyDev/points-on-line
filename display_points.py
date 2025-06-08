b0a2890f129c719bebb0945572a00c49f056e0f4
MevyDev <mevydev@proton.me>
2025-06-08 14:42:01 +0200

    feat: dynamically update dot radius based on grid size

--- display_points.py
+++ display_points.py
@@ -17,16 +17,30 @@ def draw_points(grid_size, points, lines):
     )
     canvas.pack()
 
+    radius = max(
+        1, (min(WINDOW_SIZE[0] // grid_size[0], WINDOW_SIZE[1] // grid_size[1])) // 2
+    )
+
     for x, y in lines:
         rel_x = round((x / grid_size[0]) * WINDOW_SIZE[0]) + WINDOW_MARGIN
         rel_y = round((y / grid_size[1]) * WINDOW_SIZE[1]) + WINDOW_MARGIN
         if (x, y) in points:
             canvas.create_oval(
-                rel_x - 2, rel_y - 2, rel_x + 2, rel_y + 2, fill="red", outline="red"
+                rel_x - radius,
+                rel_y - radius,
+                rel_x + radius,
+                rel_y + radius,
+                fill="red",
+                outline="red",
             )
             continue
         canvas.create_oval(
-            rel_x - 2, rel_y - 2, rel_x + 2, rel_y + 2, fill="black", outline="black"
+            rel_x - radius,
+            rel_y - radius,
+            rel_x + radius,
+            rel_y + radius,
+            fill="black",
+            outline="black",
         )
 
     root.mainloop()
